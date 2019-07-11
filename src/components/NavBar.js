5ed54c00bd6aebc1d666e5b1097ea4054d0b89ea
github username <github email address>
2019-07-11 16:19:43 -0700

    sockets work, but only for 1 room

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,9 @@ class NavBar extends Component {
                 <div className="content">
                     <Route exact path="/signup" component={SignUp} />
                     <Route exact path="/login" component={Login} />
-                    <Route path="/documents" render={routerProps => <DocumentsPage {...routerProps} documents={this.props.documents}/>} /> 
+                    <Route exact path="/documents" render={routerProps => 
+                        <DocumentsPage {...routerProps} documents={this.props.documents}/>
+                    }/> 
                 </div>
             </BrowserRouter>
         )
--- src/containers/DocumentsPage.js
+++ src/containers/DocumentsPage.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
 import WorkSpaceCard from '../components/WorkSpaceCard'
 
 
-const DocumentsShow = ({match, documents}) => {
-  console.log(documents)
-  return (
-    <div>
-      {documents.map(document => (
-      	<Route key={document.extension} 
-      	path={`${match.url}/${document.extension}`} 
-      	document={document}
-      	render={routerProps => 
-      		<WorkSpaceCard document={document} /> } />
-	  ))}
-    </div>
-  );
+class DocumentsShow extends Component {
+  render() {
+    const {match, documents} = this.props
+    return (
+      <div className="documentsList">
+        {documents.map(document => (
+        	<Route key={document.extension} 
+        	path={`${match.url}/${document.extension}`} 
+        	render={routerProps => 
+        		<WorkSpaceCard document={document} /> } />
+  	    ))}
+      </div>
+    );
+  }
 }
 
 export default DocumentsShow;
