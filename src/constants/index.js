ab8b45fcc51d8246d67edbca23735b3dc55cdfe4
jkvyff <github email address>
2019-10-22 17:56:35 -0700

    heroku deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const API_ROOT = 'https://workspace-project.herokuapp.com/api/v1';
 export const API_DOC = 'https://workspace-project.herokuapp.com/api/v1/documents';
 // export const API_WS_ROOT = 'ws://localhost:3000/api/v1/cable';
 export const API_WS_ROOT = 'ws://workspace-project.herokuapp.com/api/v1/cable'
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'http://localhost:3000';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
