acca5ab472e8b7e10829067bed3dce72bbae6e63
jkvyff <kristianvyff@gmail.com>
2020-01-30 15:39:51 -0800

    test implement full rich text on heroku

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,26 +1,241 @@
-import React, { useState, useMemo } from 'react'
-import { createEditor } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
+import React, { useCallback, useMemo, useState } from 'react'
+import { API_WS_ROOT } from '../constants';
+import ActionCable from 'actioncable';
+import isHotkey from 'is-hotkey'
+import { Editable, withReact, useSlate, Slate } from 'slate-react'
+import { Editor, Transforms, createEditor } from 'slate'
 import { withHistory } from 'slate-history'
 
+import { Button, Icon, Toolbar } from './TextEditor-Utils'
 
-const TextEditor = () => {
+const HOTKEYS = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+}
+
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+
+const TextEditor = (text, document) => {
     const [value, setValue] = useState(initialValue)
+    const renderElement = useCallback(props => <Element {...props} />, [])
+    const renderLeaf = useCallback(props => <Leaf {...props} />, [])
     const editor = useMemo(() => withHistory(withReact(createEditor())), [])
 
+    const cable = ActionCable.createConsumer(API_WS_ROOT)
+
+    const handleReceiveNewText = (doc) => {
+        console.log(doc.timestamp, 'received', doc.content)
+        if (doc.content !== JSON.stringify(value) && doc.id === document.id && doc.timestamp >= lastPress) {
+            setValue(JSON.parse(doc.content))
+        }
+    }
+
+    let sub = cable.subscriptions.create('DocumentsChannel', {
+        received: handleReceiveNewText
+    })
+
+    let lastTimer = null
+    let lastPress = 0
+
+    const handleChange = (value) => {
+        setValue(value)
+        // figure out the milliseconds since last keypress
+        let now = (new Date()).getTime()
+        let delta = now - lastPress
+        lastPress = now
+
+        // if the last time we pressed a key was recent then cancel the update to the server
+        if (delta < 500) {
+            clearTimeout(lastTimer)
+        }
+        // always assume we will send a update to the server 500ms from now
+        let content = JSON.stringify(value)
+        lastTimer = setTimeout(() => {
+            sub.send({ content, id: document.id, timestamp: now })           
+        })
+    }
+
     return (
-        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-            <Editable placeholder="Enter some plain text..." />
-        </Slate>
+    <Slate editor={editor} value={value} onChange={handleChange}>
+        <Toolbar>
+        <MarkButton format="bold" icon="format_bold" />
+        <MarkButton format="italic" icon="format_italic" />
+        <MarkButton format="underline" icon="format_underlined" />
+        <MarkButton format="code" icon="code" />
+        <BlockButton format="heading-one" icon="looks_one" />
+        <BlockButton format="heading-two" icon="looks_two" />
+        <BlockButton format="block-quote" icon="format_quote" />
+        <BlockButton format="numbered-list" icon="format_list_numbered" />
+        <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+        </Toolbar>
+        <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        placeholder="Enter some rich text…"
+        spellCheck
+        autoFocus
+        onKeyDown={event => {
+            for (const hotkey in HOTKEYS) {
+            if (isHotkey(hotkey, event)) {
+                event.preventDefault()
+                const mark = HOTKEYS[hotkey]
+                toggleMark(editor, mark)
+            }
+            }
+        }}
+        />
+    </Slate>
     )
 }
 
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format)
+  const isList = LIST_TYPES.includes(format)
+
+  Transforms.unwrapNodes(editor, {
+    match: n => LIST_TYPES.includes(n.type),
+    split: true,
+  })
+
+  Transforms.setNodes(editor, {
+    type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+  })
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] }
+    Transforms.wrapNodes(editor, block)
+  }
+}
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format)
+
+  if (isActive) {
+    Editor.removeMark(editor, format)
+  } else {
+    Editor.addMark(editor, format, true)
+  }
+}
+
+const isBlockActive = (editor, format) => {
+  const [match] = Editor.nodes(editor, {
+    match: n => n.type === format,
+  })
+
+  return !!match
+}
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor)
+  return marks ? marks[format] === true : false
+}
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case 'block-quote':
+      return <blockquote {...attributes}>{children}</blockquote>
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>
+    case 'list-item':
+      return <li {...attributes}>{children}</li>
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>
+    default:
+      return <p {...attributes}>{children}</p>
+  }
+}
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
+
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isBlockActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
+
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
+
 const initialValue = [
-    {
-        children: [
-            { text: 'Enter some plain text Whoot...' },
-        ],
-    },
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'Try it out for yourself!' }],
+  },
 ]
 
 export default TextEditor
--- src/components/WorkSpaceCard.js
+++ src/components/WorkSpaceCard.js
@@ -1,48 +1,9 @@
 import React, { Component } from 'react';
-import { API_WS_ROOT } from '../constants';
-import TextEditor from '../components/TextEditor'
-import ActionCable from 'actioncable';
+import TextEditor from './TextEditor'
 
 class WorkSpaceCard extends Component {
-    lastTimer = null
-    lastPress = 0
-
     state = {
         content: ""
-    }
-
-    componentDidMount() {
-        this.setState({content: this.props.document.content})
-
-        const cable = ActionCable.createConsumer(API_WS_ROOT)
-        this.sub = cable.subscriptions.create('DocumentsChannel', {
-            received: this.handleReceiveNewText
-        })
-    }
-
-    handleReceiveNewText = (doc) => {
-        console.log(doc.timestamp, 'received', doc.content)
-        if (doc.content !== this.state.content && doc.id === this.props.document.id && doc.timestamp >= this.lastPress) {
-            this.setState({ content: doc.content })
-        }
-    }
-    
-    handleChange = (ev) => {
-        this.setState({ content: ev.target.value })
-        // figure out the milliseconds since last keypress
-        let now = (new Date()).getTime()
-        let delta = now - this.lastPress
-        this.lastPress = now
-
-        // if the last time we pressed a key was recent then cancel the update to the server
-        if (delta < 500) {
-            clearTimeout(this.lastTimer)
-        }
-        // always assume we will send a update to the server 500ms from now
-        let content = ev.target.value
-        this.lastTimer = setTimeout(() => {
-            this.sub.send({ content, id: this.props.document.id, timestamp: now })           
-        })
     }
 
     render() {
@@ -50,13 +11,9 @@ class WorkSpaceCard extends Component {
         return (
             <div>
                 <h3>WorkSpace {extension} </h3>
-                {/* <TextEditor text={this.state.content} /> */}
-                <textarea className="work-space"
-                    rows="15" 
-                    placeholder={"Write in the Workspace Here..."}
-                    onChange={this.handleChange}
-                    value={this.state.content}
-                ></textarea>
+                <TextEditor document={this.props.document}
+                            text={this.state.content}
+                            handleChange={this.handleChange} />
             </div>
         ) 
     }
